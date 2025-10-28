deca0f1aa24c880a0a618b6b40baef8920c6c99c
shutakabeya <varane.sk@gmail.com>
2025-10-28 14:40:21 +0900

    fix: 商品ページのボタンテキストを「商品をサイトで見る」に変更

--- src/app/suggestion/[state_id]/page.tsx
+++ src/app/suggestion/[state_id]/page.tsx
@@ -192,7 +192,7 @@ export default function SuggestionPage() {
                     onClick={() => handleProductClick(product)}
                     className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg font-medium hover:bg-blue-700 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
                   >
-                    Amazonで見る
+                    商品をサイトで見る
                   </button>
                 </div>
               </div>
